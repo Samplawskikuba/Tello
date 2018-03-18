f1cfe81d245537f0df74d169e3b659f2d727324d
Jakub Samplawski <lebronija23@gmail.com>
2018-03-18 13:06:09 +0100

    tello

---
+++ Assets/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	bool MoveImage;
+
+	[Tooltip("Put title here, needs to be 500x200")]
+	public GameObject MovableTitle;
+
+
+	[Range(1,10)]
+	public float MoveSpeed;
+
+	float timer;
+	
+	// Update is called once per frame
+	void Update () {
+		if (MoveImage) {
+			MovableTitle.transform.Translate (Vector3.left * Time.deltaTime * MoveSpeed * 100f);
+		}
+		timer += Time.deltaTime;
+
+		if (MovableTitle.transform.localPosition.x <= 0) {
+			MoveImage = false;
+		}
+	}
+
+	public void NewImage(){
+		timer = 0;
+		MoveImage = true;
+
+	}
+}
